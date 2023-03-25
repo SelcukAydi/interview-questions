be46154d4c83bca45eb43039987f212416887d7b
selcuk.aydi <selcuk.aydi@afiniti.com>
2023-03-25 22:57:50 +0300

    Update

--- distinct-items-k-frame.cpp
+++ distinct-items-k-frame.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <array>
 #include <unordered_set>
+#include <algorithm>
 
 void printDistinctItemsInFrame(const std::vector<int>& arr, int k)
 {
@@ -73,14 +74,44 @@ void printDistinctItemsInFrame(const std::vector<int>& arr, int k)
     std::puts("");
 }
 
+void printSet(const std::unordered_set<int>& uniques)
+{
+    std::for_each(uniques.cbegin(), uniques.cend(), [](int item) -> void { std::cout << item << ' '; });
+    std::puts("");
+}
+
+void findDistinctItems(const std::vector<int>& arr, int k)
+{
+    std::unordered_set<int> uniques;
+    int p = k, n = arr.size();
+    
+    for(int i = 0; i < k; ++i)
+    {
+        uniques.emplace(arr[i]);
+    }
+
+    printSet(uniques);
+
+    while(p < n)
+    {
+        if(uniques.count(arr[p - k]) == 0)
+            uniques.insert(arr[p - k]);
+        else uniques.erase(arr[p - k]);
+        if(uniques.count(arr[p]) > 0)
+            uniques.erase(arr[p]);
+        else uniques.insert(arr[p]);
+        printSet(uniques);
+        ++p;
+    }
+}
 
 int main(int argc, char const *argv[])
 {
     std::vector<int> arr1 = { 1,2,3,4,4,5,6,7,3,4,4,2,1 };
 
     printDistinctItemsInFrame(arr1, 4);
-
-
+    std::puts("========================================");
+    findDistinctItems(arr1, 4);
 
     return 0;
 }
